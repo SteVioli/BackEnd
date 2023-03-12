81893fdaba183bbd6d9e97dc1498c3897e73f400
SteVioli <115998712+SteVioli@users.noreply.github.com>
2023-03-12 18:12:00 +0100

    Delete GestioneDispositiviCompleto directory

--- GestioneDispositiviCompleto/src/main/java/com/Stefano_GestioneDispositivi/models/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.Stefano_GestioneDispositivi.models;
-
-public enum ERole {
-	ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_MODERATOR;
-}
--- GestioneDispositiviCompleto/src/main/java/com/Stefano_GestioneDispositivi/models/StatoDispositivo.java
+++
@@ -1,8 +0,0 @@
-package com.Stefano_GestioneDispositivi.models;
-
-public enum StatoDispositivo {
-	DISPONIBILE,
-	ASSEGNATO,
-	IN_MANUTENZIONE,
-	DISMESSO
-}
